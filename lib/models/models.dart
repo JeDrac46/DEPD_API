17133eb26d8086d8e08edf5b222db37bdf2cc9bd
JeDrac46 <92147852+Jeshaiah04@users.noreply.github.com>
2023-12-10 19:16:31 +0700

    Last Update

---
+++ lib/models/models.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+
+import 'package:equatable/equatable.dart';
+
+part 'city.dart';
+// part 'cost.dart';
+part 'costs.dart';
+part 'province.dart';
---
+++ lib/views/pages/pages.dart
@@ -0,0 +1,6 @@
+import 'package:api_exercise/models/models.dart';
+import 'package:api_exercise/services/services.dart';
+import 'package:api_exercise/views/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+part 'home.dart';
